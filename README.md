4c0c1f81d31d35563c560bba353ff5560fa38fb2
Roman Osadko <osadko03@gmail.com>
2025-07-31 12:33:57 +0300

    finished todo app CRUD

--- README.md
+++ README.md
@@ -1,9 +1,1 @@
-# React TypeScript Starter Pack
-
-To use this template click `Use this template`
-
-### Available Scripts
-
-`Deploy` - available to deploy your application to gh-pages, to deploy the project, change the second line of code in package.json, specifically the homepage value, from "." to the repository name
-
-`SCSS Preprocessor` - available to write your styles with modern style language
+- [DEMO LINK](https://tempiqq.github.io/react-typescript-starter-pack/)
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,124 @@
-import React from 'react';
-import './App.scss';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/control-has-associated-label */
+import React, { useState } from 'react';
+import { UserWarning } from './UserWarning';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
+import * as todoServices from './api/todos';
+import { FilterStatus } from './utils/FilterStatus';
 
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { TodoFooter } from './components/TodoFooter';
+import { TodoHeader } from './components/TodoHeader';
+import { ErrorNotification } from './components/ErrorNotification';
+import { TodoItem } from './components/TodoItem';
+import { TodoList } from './components/TodoList';
+import { ErrorMessage } from './utils/ErrorMessage';
+import { useTodo } from './hooks/useTodos';
 
 export const App: React.FC = () => {
+  const [filter, setFilter] = useState<FilterStatus>(FilterStatus.All);
+
+  const {
+    // state
+    todos,
+    isLoading,
+    newTodoTitle,
+    setNewTodoTitle,
+    tempTodo,
+    errorMessage,
+    setErrorMessage,
+    editingTodoId,
+    editingTitle,
+    setEditingTitle,
+    processingTodoIds,
+
+    // values
+    isAddingTodo,
+    activeTodosCount,
+    showFooter,
+    isProcessingAnyTodo,
+    everyTodosCompleted,
+
+    // functions
+    handleAddTodo,
+    handleDeleteTodo,
+    handleClearCompleted,
+    handleUpdateTodo,
+    handleToggleAllTodos,
+    handleStartEditing,
+    handleCancelEdit,
+    handleSaveEdit,
+  } = useTodo();
+
+  if (!todoServices.USER_ID) {
+    return <UserWarning />;
+  }
+
+  const visibleTodos = todos.filter(todo => {
+    switch (filter) {
+      case FilterStatus.Active:
+        return !todo.completed;
+      case FilterStatus.Completed:
+        return todo.completed;
+      case FilterStatus.All:
+      default:
+        return true;
+    }
+  });
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <TodoHeader
+          onAddTodo={handleAddTodo}
+          newTodoTitle={newTodoTitle}
+          setNewTodoTitle={setNewTodoTitle}
+          isAddingTodo={isAddingTodo}
+          isProcessingAnyTodo={isProcessingAnyTodo}
+          everyTodosCompleted={everyTodosCompleted}
+          onToggleAllTodos={handleToggleAllTodos}
+          isTogglingAllTodos={isProcessingAnyTodo}
+          hasTodos={todos.length > 0}
+          isLoading={isLoading}
+        />
+        <TodoList
+          todos={visibleTodos}
+          onDeleteTodo={handleDeleteTodo}
+          onToggleTodo={handleUpdateTodo}
+          editingTodoId={editingTodoId}
+          editingTitle={editingTitle}
+          onStartEdit={handleStartEditing}
+          onCancelEdit={handleCancelEdit}
+          onSaveEdit={handleSaveEdit}
+          setEditingTitle={setEditingTitle}
+          processingTodoIds={processingTodoIds}
+        />
+
+        {isAddingTodo && tempTodo && (
+          <TodoItem
+            key="temp-todo-loader"
+            todo={tempTodo}
+            isTemp={true}
+            processingWithTodos={true}
+          />
+        )}
+
+        {showFooter && (
+          <TodoFooter
+            activeTodosCount={activeTodosCount}
+            filter={filter}
+            setFilter={setFilter}
+            onClearCompleted={handleClearCompleted}
+            totalTodos={todos.length}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        message={errorMessage}
+        onClose={() => setErrorMessage(ErrorMessage.DEFAULT_ERROR)}
+      />
     </div>
   );
 };
